c20a1c6f44d1645cefc826d54ba9ec973a3680a3
gerlandomarotta <42580101+gerlandomarotta@users.noreply.github.com>
2025-02-18 10:54:53 +0000

    Update test_reverse

--- tests/gmarotta/test_reverse.py
+++ tests/gmarotta/test_reverse.py
@@ -3,5 +3,14 @@ sys.path.append("packages/gmarotta/reverse")
 import reverse
 
 def test_reverse():
-    res = reverse.reverse({})
-    assert res["output"] == "reverse"
+    args = {}
+    res = reverse.reverse(args)
+    assert res["output"] == "Plese provide some input"
+
+    args = {"input": "hello"}
+    res = reverse.reverse(args)
+    assert res["output"] == "olleh"
+    
+    args = {"input": "ciao"}
+    res = reverse.reverse(args)
+    assert res.get("output") == "oaic"
--- tests/gmarotta/test_reverse_int.py
+++ tests/gmarotta/test_reverse_int.py
@@ -2,4 +2,7 @@ import os, requests as req
 def test_reverse():
     url = os.environ.get("OPSDEV_HOST") + "/api/my/gmarotta/reverse"
     res = req.get(url).json()
-    assert res.get("output") == "reverse"
+    assert res.get("output") == "Plese provide some input"
+
+    res = req.post(url, json={"input": "hello"}).json()
+    assert res.get("output") == "olleh"
